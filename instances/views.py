99bce442c3c6d4dce50dfca71decbc08c1c8eed1
Сергеев Игнатий <114506759+IgnatSergeev@users.noreply.github.com>
2024-02-15 18:12:25 +0300

    Updated owns

--- instances/views.py
+++ instances/views.py
@@ -151,7 +151,7 @@ def instance(request, pk):
         instance.drbd = drbd_status(request, pk)
         instance.save()
 
-    user_owns_instance = instances =  instance in Instance.objects.filter(userinstance__user=request.user)
+    user_owns_instance = instance in Instance.objects.filter(userinstance__user=request.user)
   
     return render(request, "instance.html", locals(),)
 
